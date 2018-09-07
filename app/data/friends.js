219ed4fc3810efd32d493b6a8caa66bca2b285b3
Jcontratto <jmc22@hotmail.com>
2018-09-07 19:05:41 -0400

    heroku commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@
 var friendList = [
     {
       name: "John Contatto",
-      profilePic: '',
+      profilePic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
       scores: [
         "4",
         "5",
